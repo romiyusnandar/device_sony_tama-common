488af2562ed8e3f5217c9cd1129ec2e6502185ef
KanonifyX <153967762+KanonifyX@users.noreply.github.com>
2025-04-22 14:35:50 +0000

    tama-common: Switch to UClamp
    *switch from stune boost to util clamp
    *added cgroup and task_profile json
    
    Signed-off-by: kanonifyX <kanonify01@gmail.com>

--- common.mk
+++ common.mk
@@ -344,6 +344,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
